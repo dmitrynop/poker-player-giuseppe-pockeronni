ce5a227c6e60da51d10d460fe014850db2ceab3d
Dmitry Nikitin <dmitry.nop@gmail.com>
2016-04-02 15:18:32 +0600

    fix second hand

--- second_hand.rb
+++ second_hand.rb
@@ -11,10 +11,10 @@ class SecondHand
     # check all cards
     response = Ranking.check_hand(@cards)
     p response
-    p response['rank']
-    if response['rank'] > 4
+    p response[:rank]
+    if response[:rank] > 4
       :raise
-    elsif response['rank'] > 1
+    elsif response[:rank] > 1
       :call
     else
       :pass
--- second_hand_test.rb
+++ second_hand_test.rb
@@ -7,4 +7,4 @@ require 'second_hand'
 p SecondHand.new([
   {"rank"=>"A", "suit"=>"spades"}, {"rank"=>"A", "suit"=>"hearts"}],
   [{"rank"=>"Q", "suit"=>"spades"}, {"rank"=>"Q", "suit"=>"hearts"}, {"rank"=>"7", "suit"=>"hearts"}
-]).good?
+]).decision
