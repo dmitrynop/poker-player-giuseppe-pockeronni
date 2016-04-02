b61ecdb2b13e1063180ac69339bd1d2cf47c2898
Artem Khramov <akhramov@thumbtack.net>
2016-04-02 13:09:11 +0600

    half instead of full-in

--- player.rb
+++ player.rb
@@ -1,7 +1,8 @@
 
 class Player
-
   VERSION = "Ovik"
+
+  MINIMUM_BET = 240
   attr_reader :state
 
   def bet_request(game_state)
@@ -11,7 +12,7 @@ class Player
     when 0
       state['current_buy_in']
     else
-      me['stack']
+      smart_bet
     end
   end
 
@@ -22,7 +23,17 @@ class Player
   private
 
   def me
+    return @me if @me
+
     my_id = state['in_action']
-    state['players'].find { |player| player['id'] == my_id }
+    @me = state['players'].find { |player| player['id'] == my_id }
+  end
+
+  def smart_bet
+    [minimum_bet, me['stack'] / 2].max
+  end
+
+  def minimum_bet
+    @minimum_bet ||= state['current_buy_in'] - me['bet'] + MINIMUM_BET
   end
 end
