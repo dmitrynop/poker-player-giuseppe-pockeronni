e64457fcdbdc9a0182b662c523b2e19171e2cb3f
Dmitry Nikitin <dmitry.nop@gmail.com>
2016-04-02 13:22:40 +0600

    update ranking code - return hash with doc

--- ranking.rb
+++ ranking.rb
@@ -2,6 +2,8 @@ require "poker_ranking"
 require "json"
 
 class Ranking
+  # input :
+  # e.g : [{:rank=>"5", :suit=>"diamonds"}, {:rank=>"6", :suit=>"diamonds"}, {:rank=>"7", :suit=>"diamonds"}, {:rank=>"7", :suit=>"spades"}, {:rank=>"8", :suit=>"diamonds"}, {:rank=>"9", :suit=>"diamonds"}]
   # cards=[
   #  {"rank":"5","suit":"diamonds"},
   #  {"rank":"6","suit":"diamonds"},
@@ -11,11 +13,21 @@ class Ranking
   #  {"rank":"9","suit":"diamonds"}
   # ]
 
+  # returns :
+  # {
+  #   :rank=>8,
+  #   :value=>9,
+  #   :second_value=>9,
+  #   :kickers=>[],
+  #   :cards_used => [],
+  #   :cards => []
+  # }
   def self.check_hand(cards)
     if cards.length >= 5
-      JSON.generate PokerRanking::Hand.new(cards).data
+      data = PokerRanking::Hand.new(cards).data
+      data
     else
-      halt 400, "Bad request: not enough cards"
+      raise "not enough cards"
     end
   end
 end
