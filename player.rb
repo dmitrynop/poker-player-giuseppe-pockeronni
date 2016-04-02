8c453192404fa8d0f9ea86f7b342c73634ad71df
Artem Khramov <akhramov@thumbtack.net>
2016-04-02 12:59:30 +0600

    Dummy player implemented

--- player.rb
+++ player.rb
@@ -2,12 +2,27 @@
 class Player
 
   VERSION = "Ovik"
+  attr_reader :state
 
   def bet_request(game_state)
-    50
+    @state = game_state
+
+    case state['round']
+    when 0
+      state['current_buy_in']
+    else
+      me['stack']
+    end
   end
 
   def showdown(game_state)
 
+  end
+
+  private
+
+  def me
+    my_id = state['in_action']
+    state['players'].find { |player| player['id'] == my_id }
   end
 end
